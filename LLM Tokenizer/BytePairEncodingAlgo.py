18e79d31d9472897cd15e2a35cde8df8da23e268
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-08-22 22:57:01 -0400

    Encoding allgorithm

--- LLM Tokenizer/BytePairEncodingAlgo.py
+++ LLM Tokenizer/BytePairEncodingAlgo.py
@@ -2,3 +2,16 @@ import importlib
 import tiktoken
 
 tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello how are you? <|endoftext|> Do you like tea? <|endoftext|> In the sunlit Terraces"
+    "of someunknownPLace."
+)
+
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+
+print(integers)
+
+decoded_strings = tokenizer.decode(integers)
+
+print(decoded_strings)
