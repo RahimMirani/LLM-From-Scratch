9bddfcc30985e346bbc283ce42e0e8716e504650
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-11-11 18:47:16 -0500

    Added data and encoded it

--- LLM Tokenizer/DataLoaders.py
+++ LLM Tokenizer/DataLoaders.py
@@ -7,3 +7,11 @@ import tiktoken
 
 #Using gpt2 tokenizer model
 tokenizer = tiktoken.get_encoding("gpt2")
+
+
+with open ("the-verdict.txt", "r", encoding= "utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+#Total Vocabulary
+print(len(enc_text))
