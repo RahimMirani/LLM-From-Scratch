6a8fe2ac179b224cd5a31e9d735065c55b7ca733
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-11-12 21:37:30 -0500

    outputing the specified embedding layers and comments

--- LLM Tokenizer/embeddings.py
+++ LLM Tokenizer/embeddings.py
@@ -3,10 +3,15 @@
 import torch
 
 input_ids = torch.tensor([2,3,5,1])
+
+#Random 6 token ids
 vocab_size = 6
 output_dimension = 3 #dimension of the output vector
 
 torch.manual_seed(123)
 embedding_layer = torch.nn.Embedding(vocab_size, output_dimension)
 print(embedding_layer.weight)
+
+# Looking up particular token ids from the embedding layer, this acts as a lookup of the the ids from the embeddings layer and returns weights/vectors of that ids
+print(embedding_layer(input_ids))
     
